03784fb8922ea695f86f0e6fdf192ce34cd076ad
pmb5 <pmb5@users.noreply.github.com>
2019-03-15 12:00:04 +0000

    Change basic CRA app into iPart
    The changes needed to get the app created by: "npx create-react-app react-starter" to work within iMIS as an iPart and auto-deploying to azure hosting.

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    webpack: {
+        configure: {
+             /* Any webpack configuration options: https://webpack.js.org/configuration */
+             output: {
+                publicPath: "https://asiprostorage.blob.core.windows.net/innovations/config-demo/"
+            },
+         }
+    }
+}
