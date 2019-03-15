ce912fdb6d1f3535e8a8e2d17c412b057b118c71
pmb5 <pmb5@users.noreply.github.com>
2019-03-15 22:05:23 +0000

    changed path in craco

--- craco.config.js
+++ craco.config.js
@@ -4,7 +4,7 @@ module.exports = {
         configure: {
              /* Any webpack configuration options: https://webpack.js.org/configuration */
              output: {
-                publicPath: "https://asiprostorage.blob.core.windows.net/innovations/config-demo/"
+                publicPath: "https://asiprostorage.blob.core.windows.net/innovations/react-starter/"
             },
          }
     }
